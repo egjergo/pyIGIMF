3587251532104f32fe972d58e397cb049fa561c2
alegresor <agsorokin3@gmail.com>
2022-12-15 00:57:49 -0600

    read_grids example with FI

--- read_grids.py
+++ read_grids.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from igimf import plots as plts
+from FriendlyInterpolants import LinearAndNearestNeighbor_FI
 plots = plts.Plots()
 
 txts = glob.glob('grid/resolution50/*pkl')
@@ -13,20 +14,36 @@ txts = glob.glob('grid/resolution50/*pkl')
 df = pd.DataFrame({col:[] for col in ['SFR', 'metal_mass_fraction',
                                       'mass_star', 'IGIMF']})
 
-for txt in txts:
+for i,txt in enumerate(txts):
     df_txt = pickle.load(open(txt, 'rb'))
     df = df.append(df_txt)
+    if i==5: break # TO BE REMOVED IN THE FUTURE!!!
+df.dropna(inplace=True) # some rows have na values
+df.index = np.arange(len(df)) # avoid repeated index from appending dataframes
+print('df.shape: %s\n'%str(df.shape))
+print(df.head())
+
+fi_igimf_full = LinearAndNearestNeighbor_FI(
+    df = df,
+    tf_funs = {},
+    xcols = ['SFR','metal_mass_fraction','mass_star'],
+    ycol = 'IGIMF',
+    name = 'fi_igimf_full')
+print(fi_igimf_full)
 
 SFR_v = np.unique(df['SFR'])
 metal_mass_fraction_v = np.unique(df['metal_mass_fraction'])
 mstar_v = np.unique(df['mass_star'])
 
-#Retrieve a value
-df.loc[(df['SFR']==SFR_v[0]) & 
-       (df['metal_mass_fraction']==metal_mass_fraction_v[0])]
-
-#get_IGIMF = IGIMF_interpolation({'metal_mass_fraction':0.0134, 'SFR':1})
-#IGIMF = get_IGIMF(mass_star)
+# Retrieve a value
+fi_igimf_marginal = lambda mass_star: fi_igimf_full(pd.DataFrame({
+    'mass_star': np.array(mass_star),
+    'metal_mass_fraction': 0.0134,
+    'SFR': 1}))
+mass_star_query = df[(df['SFR']==df['SFR'][0])&(df['metal_mass_fraction']==df['metal_mass_fraction'][0])]['mass_star']
+yhat = fi_igimf_marginal(mass_star=mass_star_query)
+print(yhat)
+import sys; sys.exit(0)
 
 #plots.sIMF_subplot(metal_mass_fraction_v, self.Mecl_v, self.mstar_v, self.IMF_Z_v_list)
 ## No IMF for SFR < 5e-5, i.e. Migal ~<6e5
